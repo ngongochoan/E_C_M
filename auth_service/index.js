e8e8e18cfed9a474a305677dd91e51f1d8c078f0
ngongochoan <57170393+ngongochoan@users.noreply.github.com>
2022-07-13 11:22:05 +0700

    nnn

---
+++ auth_service/index.js
@@ -0,0 +1,58 @@
+const express=require("express");
+const app=express();
+const PORT=process.env.PORT_ONE || 7070;
+const User = require("./User");
+const mongoose=require("mongoose");
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+mongoose.connect("mongodb:// localhost/auth-service",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},() =>{
+    console.log("Auth-Service DB Connected");
+});
+
+
+//Register
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.json({ message: "User doesn't exist" });
+    } else 
+    {
+        // if (password !== user.password) {
+        //     return res.json({ message: "Password Incorrect" });
+        // }
+        const payload = {
+            email,
+            name: user.name
+        };
+        jwt.sign(payload, "secret", (err, token) => {
+            if (err) console.log(err);
+            else return res.json({ token: token });
+        });
+    }
+});
+app.post("/auth/register", async (req, res) => {
+    const { email, password, name } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.json({ message: "User already exists" });
+    } else {
+        const newUser = new User({
+            email,
+            name,
+            password,
+        });
+        newUser.save();
+        return res.json(newUser);
+    }
+});
+//Login
+
+
+app.listen(PORT,()=>{
+    console.log(`Auth-Service at${PORT}`);
+});
+    
